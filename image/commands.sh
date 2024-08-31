2f5133396846b9964e132bfe45a2978c3c95147f
ReitererLukas <lukas-reiterer@gmx.at>
2024-08-31 14:48:23 +0200

    custom playlist

--- image/commands.sh
+++ image/commands.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cp /trackmania_config/dedicated_cfg.txt /trackmania/TMServer/GameData/Config/dedicated_cfg.txt
-./TrackmaniaServer /game_settings=MatchSettings/Nations/NationsBlue.txt /dedicated_cfg=dedicated_cfg.txt
+cp /trackmania_config/kg.txt /trackmania/TMServer/GameData/Tracks/MatchSettings/Nations/kg.txt
+
+./TrackmaniaServer /game_settings=MatchSettings/Nations/kg.txt /dedicated_cfg=dedicated_cfg.txt /verbose_rpc_full 
 
 if [ -d "/trackmania/xaseco" ]; then
     cp /trackmania_config/*.xml /trackmania/xaseco/
